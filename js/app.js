c4f8f7d014180e775ab01246c8e60e834544344d
vimalpatel87 <vimal.161187@gmail.com>
2017-12-16 12:04:59 -0600

    added sorting and direction

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 var myApp = angular.module('myApp',[]);
 myApp.controller('MyController', function MyController($scope,$http) {
+    $scope.propertyOrder = 'name';
     $http.get('js/data.json').then(function(response){
        $scope.properties = response.data; 
     });
