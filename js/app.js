cef54bdcecaf4da8dc172cdb2deaff1d3088a6e8
vimalpatel87 <vimal.161187@gmail.com>
2017-12-15 22:48:46 -0600

    Used http service to get JSON Data

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 var myApp = angular.module('myApp',[]);
-myApp.controller('MyController', function MyController($scope) {
-    $scope.property = {
-    "name":"Sky Harbour Estates",
-    "shortname":"sky_harbour_estates",
-    "reknown":"Sky Harbour Estates Regina",
-    "bio":"Sky Harbour Estates is a 314 unit Altern community located in Harbour Landing that backs on to recreational green space. Within the complex there are spacious 1 and 2 bedroom suites that feature modern finishes and spacious floor plans. All suites come equipped with air conditioning, full sized appliances (including dishwasher and microwave) and in suite laundry!"
-  };
+myApp.controller('MyController', function MyController($scope,$http) {
+    $http.get('js/data.json').then(function(response){
+       $scope.properties = response.data; 
+    });
 });
